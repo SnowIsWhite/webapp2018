7e1c177a2cfdc4a93208ca749704c57f9466b7bb
SnowIsWhite <jibae617@gmail.com>
2018-06-19 18:56:30 +0900

    chaged db creation for english filenames

--- avagai/utility/item-img2db.py
+++ avagai/utility/item-img2db.py
@@ -1,21 +1,23 @@
 import os
 
-target_folder_dir = '../public/img/wanted'
-save_dir = './wanted_db.txt'
+target_folder_dir = '../public/img/having'
+save_dir = './having_db.txt'
 
 with open(save_dir, 'w') as f:
     f.write('')
 
-def make_query(fname, ftype):
-    query = 'INSERT INTO wanted_items(file_name, file_type) VALUES("{}","{}");';
-    query = query.format(fname, ftype)
-
+def make_query(fname, ftype, cnt):
+    query = 'INSERT INTO having_items(file_name, renamed, file_type) VALUES("{}","having-{}","{}");';
+    query = query.format(fname, cnt, ftype)
     with open(save_dir, 'a') as f:
         f.write(query+'\n')
 
-i = 0
+cnt = 0
 for path, dir, files in os.walk(target_folder_dir):
     for f in files:
+        cnt += 1
         fname = f.split('.')[0]
         ftype = f.split('.')[-1]
-        make_query(fname, ftype)
+        make_query(fname, ftype, str(cnt))
+        # change file name
+        os.rename(os.path.join(target_folder_dir, f), os.path.join(target_folder_dir, 'having-{}.{}'.format(str(cnt), ftype)))
